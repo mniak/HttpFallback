2544bfe2de6d528d52ef242dd06f670db51994e5
Andre Guilherme <andre.guilherme@iatec.com>
2017-06-21 11:29:32 -0300

    Add Extension method

---
+++ src/HttpFallback.Owin/HttpFallbackOwinExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using System.Net;
+using HttpFallback.Owin;
+
+namespace Owin
+{
+    public static class HttpFallbackOwinExtensions
+    {
+        public static IAppBuilder UseHttpFallback(this IAppBuilder app, int code)
+        {
+            app.Use<HttpFallbackOwinMiddleware>(code);
+            return app;
+        }
+        public static IAppBuilder UseHttpFallback(this IAppBuilder app, HttpStatusCode statusCode)
+        {
+            app.UseHttpFallback((int)statusCode);
+            return app;
+        }
+    }
+}
